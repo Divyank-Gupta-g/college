f98dcaa4941cc9f119f5ae064a9d2262838e3aff
Divyank-Gupta-g <divyankgupta001@gmail.com>
2023-10-04 17:59:25 +0530

    Succesful

---
+++ src/collegemanagement/CollegeManagement.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package collegemanagement;
+
+/**
+ *
+ * @author DELL
+ */
+public class CollegeManagement {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
